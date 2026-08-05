bc4e47769e1b8499d0ac6d6e31faf40635a88eda
Abhinav Jain <abhinav.ajay03@gmail.com>
2026-08-05 16:27:03 +0530

    Implement Fire Wall and Space Man mechanics in Abhi scene

---
+++ Assets/_CyberRakshak/Scripts/WaterObject.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace CyberRakshak
+{
+    /// <summary>
+    /// Attach this script to any environment object that acts as water to extinguish the FirewallHazard.
+    /// </summary>
+    public class WaterObject : MonoBehaviour
+    {
+        // This is a marker component.
+    }
+}
