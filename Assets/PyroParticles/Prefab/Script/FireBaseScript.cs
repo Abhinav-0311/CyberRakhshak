6fa3a4c55ae89c4716bdfeabf437d278eb31ab1b
Abhinav Jain <abhinav.ajay03@gmail.com>
2026-08-10 21:28:03 +0530

    Replace default player and camera with Aditya prefab in all gameplay scenes

--- Assets/PyroParticles/Prefab/Script/FireBaseScript.cs
+++ Assets/PyroParticles/Prefab/Script/FireBaseScript.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 namespace DigitalRuby.PyroParticles
@@ -81,7 +81,10 @@ namespace DigitalRuby.PyroParticles
         {
             Starting = true;
             int fireLayer = UnityEngine.LayerMask.NameToLayer("FireLayer");
-            UnityEngine.Physics.IgnoreLayerCollision(fireLayer, fireLayer);
+            if (fireLayer >= 0)
+            {
+                UnityEngine.Physics.IgnoreLayerCollision(fireLayer, fireLayer);
+            }
         }
 
         protected virtual void Start()
